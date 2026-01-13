51fa86e31ec0776dc4d03cd2c5c1961905b340d0
Aditya-Panda33 <adityaponda2020@gmail.com>
2026-01-13 20:28:25 +0530

    Text extraction from image

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from PIL import Image
+import pytesseract
+
+app = Flask(__name__)
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\tesseract.exe"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    extracted_text = ""
+
+    if request.method == "POST":
+        file = request.files.get("image")
+
+        if file:
+            img = Image.open(file.stream)   # <-- in memory
+            extracted_text = pytesseract.image_to_string(img)
+
+    return render_template("index.html", text=extracted_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
